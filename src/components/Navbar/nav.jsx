6fc690b5e668b1b127d497927d6539b0c3830efd
khoa <tkhoa1122@gmail.com>
2025-02-21 16:34:55 +0700

    dark-mode

--- src/components/Navbar/nav.jsx
+++ src/components/Navbar/nav.jsx
@@ -2,10 +2,25 @@ import React, { Component } from "react";
 import './nav.css'
 
 export default class Nav extends Component {
-  
+  constructor(props) {
+    super(props);
+    this.state = {
+      darkMode: false
+    };
+  }
+
+  toggleDarkMode = () => {
+    this.setState(prevState => ({
+      darkMode: !prevState.darkMode
+    }), () => {
+      document.body.classList.toggle('dark-mode', this.state.darkMode);
+      document.documentElement.classList.toggle('dark-mode', this.state.darkMode);
+    });
+  };
+
   render() {
     return (
-      <nav className="navbar navbar-expand-lg bg-body-tertiary">
+      <nav className={`navbar navbar-expand-lg ${this.state.darkMode ? 'bg-dark' : 'bg-body-tertiary'}`}>
         <div className="container-fluid">
           <div className="logo-div">
             <img
@@ -14,7 +29,7 @@ export default class Nav extends Component {
               alt="Orchid Logo"
             />
             <a
-              className="navbar-brand"
+              className={`navbar-brand ${this.state.darkMode ? 'text-light' : ''}`}
               href="#"
             >
               Orchids
@@ -34,12 +49,12 @@ export default class Nav extends Component {
           <div className="collapse navbar-collapse" id="navbarSupportedContent">
             <ul className="navbar-nav mx-auto mb-2 mb-lg-0">
               <li className="nav-item">
-                <a className="nav-link active" aria-current="page" href="#">
+                <a className={`nav-link active ${this.state.darkMode ? 'text-light' : ''}`} aria-current="page" href="#">
                   Home
                 </a>
               </li>
               <li className="nav-item">
-                <a className="nav-link" href="#">
+                <a className={`nav-link ${this.state.darkMode ? 'text-light' : ''}`} href="#">
                   Link
                 </a>
               </li>
@@ -75,21 +90,24 @@ export default class Nav extends Component {
                 </ul>
               </li>
             </ul>
-            <form
-              className="d-flex"
-              role="search"
-              style={{ marginRight: "100px" }}
-            >
-              <input
-                className="form-control me-2"
-                type="search"
-                placeholder="Search"
-                aria-label="Search"
-              />
-              <button className="btn btn-outline-success" type="submit">
-                Search
+            <div className="d-flex align-items-center">
+              <div className="form-check form-switch me-3">
+                <input
+                  className="form-check-input"
+                  type="checkbox"
+                  role="switch"
+                  id="darkModeSwitch"
+                  checked={this.state.darkMode}
+                  onChange={this.toggleDarkMode}
+                />
+                <label className={`form-check-label ${this.state.darkMode ? 'text-light' : ''}`} htmlFor="darkModeSwitch">
+                  {this.state.darkMode ? '🌙' : '☀️'}
+                </label>
+              </div>
+              <button className="btn btn-link" type="button">
+                <i className={`fas fa-user ${this.state.darkMode ? 'text-light' : ''}`} style={{ fontSize: '1.5rem' }}></i>
               </button>
-            </form>
+            </div>
           </div>
         </div>
       </nav>
